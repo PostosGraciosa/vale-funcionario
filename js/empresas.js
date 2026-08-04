549e1bca0ee3d3c90f620c8760c63d5b32ade7e4
PostosGraciosa <administracao@postosgraciosa.com.br>
2026-08-04 16:55:30 -0300

    Create empresas.js

---
+++ js/empresas.js
@@ -0,0 +1,38 @@
+const empresas = [
+
+    {
+        nome: "POSTO JARIVA LTDA",
+        cnpj: "04.123.127/0001-59"
+    },
+
+
+    {
+        nome: "AUTO POSTO FÁTIMA LTDA",
+        cnpj: "79.270.211/0001-02"
+    },
+
+
+    {
+        nome: "POSTO BEMER LTDA",
+        cnpj: "81.512.683/0001-68"
+    },
+
+
+    {
+        nome: "POSTO GRACIOSA V LTDA",
+        cnpj: "84.708.437/0001-74"
+    },
+
+
+    {
+        nome: "AUTO POSTO PIRAÍ LTDA",
+        cnpj: "11.168.652/0001-56"
+    },
+
+
+    {
+        nome: "POSTO GRACIOSA LTDA",
+        cnpj: "76.608.660/0001-11"
+    }
+
+];
